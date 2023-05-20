5b5bdde904d119d2d9476c679ea70f26a0d2af2b
Jong44 <mat_skak45@gmail.com>
2023-05-21 03:16:29 +0700

    firstcom

---
+++ src/views/Page/Home.vue
@@ -0,0 +1,16 @@
+<script>
+    import Header from "../komponen/partials/Header.vue"
+    import Footer from "../komponen/partials/Footer.vue"
+
+    export default{
+        components:{
+            Header,
+            Footer
+        }
+    }
+</script>
+
+<template>
+    <Header/>
+    <Footer/>
+</template>
---
+++ src/views/komponen/partials/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class=" absolute top-0 left-0 right-0 py-14 px-20 flex justify-between items-center">
+        <div class=" text-3xl flex gap-2">
+        <p class=" bg-[#b3e2dc] rounded-full px-2 font-bold text-white">T</p>
+        <p class=" text-gray-500">anjung</p>
+        
+        </div>
+        <div class=" mt-1 flex gap-16 text-gray-300">
+            <RouterLink to="/">About Me</RouterLink>
+            <RouterLink to="/a">Resume</RouterLink>
+            <RouterLink to="/a">Blog</RouterLink>
+            <RouterLink to="/a">Contact</RouterLink>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+
+    a{
+        transition: color 0.5s ease;
+    }
+
+    a:hover{
+        color: black;  
+    }
+    a.router-link-exact-active{
+        color: black;
+    }
+</style>
